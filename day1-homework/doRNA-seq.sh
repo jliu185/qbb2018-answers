c9c32f713859a5b238ae5020233fa76bd0cb4594
jliu185 <jliu185@jhu.edu>
2018-08-28 09:35:32 -0400

    Version 2

--- day1-homework/doRNA-seq.sh
+++ day1-homework/doRNA-seq.sh
@@ -7,9 +7,9 @@ for SAMPLE in SRR072893 SRR072903 SRR072905 SRR072915
 do
   mkdir $SAMPLE
   fastqc ~/data/rawdata/${SAMPLE}.fastq
-  hisat2 -x ${GENOME} -U ${SAMPLE}/${SAMPLE.fastq -p 4 -S ${SAMPLE}/${SAMPLE}.sam
-  samtools view -b ${SAMPLE}/${SAMPLE}.sam -o {SAMPLE}/${SAMPLE}.bam           
-  samtools sort ${SAMPLE}/${SAMPLE}.bam -o ${SAMPLE}/${SAMPLE}.bam.sort          
-  samtools index ${SAMPLE}/${SAMPLE}.bam.sort
-  stringtie -p 4 -e -B ${SAMPLE}/${SAMPLE}.bam.sort -G ${ANNOTATION} -o {SAMPLE}/${SAMPLE}.gtf
+  hisat2 -x ${GENOME} -U ~/data/rawdata/${SAMPLE}.fastq -p 4 -S ~/data/rawdata/${SAMPLE}.sam
+  samtools view -b ~/data/rawdata/${SAMPLE}.sam -o ~/data/rawdata/${SAMPLE}.bam           
+  samtools sort ~/data/rawdata/${SAMPLE}.bam -o ~/data/rawdata/${SAMPLE}.bam.sort          
+  samtools index ~/data/rawdata/${SAMPLE}.bam.sort
+  stringtie -p 4 -e -B ~/data/rawdata/${SAMPLE}.bam.sort -G ${ANNOTATION} -o ~/data/rawdata/${SAMPLE}.gtf
 done
