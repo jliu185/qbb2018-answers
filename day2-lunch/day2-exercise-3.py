9f6e0459af471c4b4542671f5e1a9e15e4e44d79
jliu185 <jliu185@jhu.edu>
2018-08-28 13:07:38 -0400

    exercise 3

---
+++ day2-lunch/day2-exercise-3.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import sys
+
+if len(sys.argv) > 1:
+    f = open ( sys.argv[1] )
+else: 
+    f = sys.stdin
+#calling the sam file
+
+count = 0    
+for i in f:  
+    if i == 0:
+        continue #skip header
+    elif "NH:i:1" in i:
+        count += 1
+
+print(count)    
