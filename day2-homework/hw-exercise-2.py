8e1757c070b85c6f746cc520924e0a2693415fb7
jliu185 <jliu185@jhu.edu>
2018-08-28 20:05:35 -0400

    homework 2b

--- day2-homework/hw-exercise-2.py
+++ day2-homework/hw-exercise-2.py
@@ -4,14 +4,28 @@
 
 import sys
 
-c_tab = []
 
-for i, line in enumerate(sys.stdin): #enumerate allows us to loop over something and have an automatic counte. 
+fly_dict = {}
+for line in open(sys.argv[1]):
+    if "_DROME" not in line:
+        continue
+    fields = line.strip("\r\n").split()
+    if fields[-1].startswith("FBgn"):
+       fly_dict.update({fields [3] : fields [2]})
+       #print (fly_dict)
+
+c_tab = []
+for i, line in enumerate(open(sys.argv[2])): #enumerate allows us to loop over something and have an automatic counte. 
     if i == 0:
         continue
     fields = line.rstrip("\r\n").split("\t")
 # THIS IS STANDARD FOR ALL TABULAR SORTING/FINDING
-    fbgn = fields[9]
-    c_tab.append(fbgn) #adds all IDs to empty list, and makes them into actual numbers
-    print(fbgn)
- 
+    fbgn = fields[8]
+    if fbgn in fly_dict.keys():
+        unip = fly_dict[fbgn]
+        print (line.rstrip('\r\n') + "\t" + unip)
+    else: 
+        print (line.rstrip('\r\n') + "\t" + "no match")
+        
+    
+        
