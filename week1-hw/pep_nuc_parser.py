437cd7c5d07621dc64684bd50f55621fe24167e1
jliu185 <jliu185@jhu.edu>
2018-09-07 20:01:53 -0400

    updated python file

--- week1-hw/pep_nuc_parser.py
+++ week1-hw/pep_nuc_parser.py
@@ -13,44 +13,24 @@ import itertools
 nuc_reader = fasta.FASTAReader(open(sys.argv[1]))
 pep_reader = fasta.FASTAReader(open(sys.argv[2]))
 
-all_nuc = [] 
-all_aa = []
+all_nuc = [] #creating a list of all the nucleotides from the for loop
+all_aa = [] #creating a list of all the aa from the for loop
 
 for (dna_id,dna),(aa_id,aa) in zip(nuc_reader,pep_reader):
-    nuc_seq = []
+    nuc_seq = [] #creating a list for the extracted nucleotides
     aa_list = []    
     j = 0
-    for i in range(len(aa)):
-        a=aa[i]
-        aa_list.append(a)
-        nuc = dna[j*3:(j+1)*3]
-        if a == "-":
-            nuc_seq.append("---")
+    for i in range(len(aa)): 
+        a=aa[i] #creating a list of all amino acid sequences through each alignment
+        aa_list.append(a) #appending that list to another list
+        nuc = dna[j*3:(j+1)*3] #counting codons per each triad of nucleotides
+        if a == "-": #blank amino acid
+            nuc_seq.append("---") #fill with 3 empty nucleotides
         else:
-            nuc_seq.append(nuc)
-            j += 1
-    all_nuc.append(nuc_seq)
-    all_aa.append(aa_list)
+            nuc_seq.append(nuc) #else append the list with the list of nucleotides 
+            j += 1 #increase the counter each time
+    all_nuc.append(nuc_seq) #appending all the nucleotide sequences with amino acid gaps 
+    all_aa.append(aa_list) #appending all the aa in a list
 
 print(all_nuc)
 print(all_aa)
-
-
-
-
-
-
-
-
-
-
-
-
-# for ident, sequences in fasta.FASTAReader(pep):
-#     pep_seq.append(sequences)
-#     fasta_ident.append(ident)
-#
-# for ident, sequences in fasta.FASTAReader(nuc):
-#     nuc_seq.append(sequences)
-#     print (nuc_seq)
-
